70ab534240d5cff1cea28d033d7f9ec085aa9987
Vorhut <thenellb@oregonstate.edu>
2017-02-28 13:22:37 -0800

    Added skeleton for class as well as empty function

---
+++ src/main/java/edu/oregonstate/cs361/battleship/CivShip.java
@@ -0,0 +1,9 @@
+package edu.oregonstate.cs361.battleship;
+
+public class CivShip extends Ship{
+
+    public void autoSink(){
+        // Function that fires at all the ship coords, sinking it. Called when any coord of civship gets hit
+    }
+
+}
